5d07cfcf069bbdc911f3db406a6684881a0f24af
kakao <kakao@kakaoui-MacBookPro.local>
2020-07-26 17:55:22 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const foodILike = [
+  {
+    id : 1,
+    name : "kimchi",
+    image : "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi-620x349.jpg",
+    rating : 2
+  },
+  {
+    id : 2,
+    name : "coke",
+    image : "https://www.battlefieldbeers.co.uk/clear_cf/pub-media-catalog-product-c-o-coke_can_2000x0.jpg",
+    rating : 2.6
+  },
+  {
+    id : 3,
+    name : "ramen",
+    image : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Shoyu_Ramen.jpg/1200px-Shoyu_Ramen.jpg",
+    rating : 2.93
+  }
+];
+
+function Food({name, image}){
+  return (
+    <div>
+     <h2>I like {name}</h2>
+     <img src={image} alt={name}></img>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(a => (
+        <Food name={a.name} image={a.image} key={a.id} /> 
+      ))}
     </div>
-  );
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 무엇을? 
+// render App from ./App, 
+// 어디에? 
+// index.html의 document.getElementById('root);
+
+
+// React 는 하나의 Component 만 Html에 Render 할 수 있다.
+// 따라서, Component 안에 또 다른 Component * n 
+// 의 형식으로 Render 하자.
+
+// ex) Potato.js 라는 Component를 추가할때. 
+// App.js > Potato.js 방식으로 import 해 추가해야 한다.
+
+// 즉, Render 되는 Component는 App.js 하나.
